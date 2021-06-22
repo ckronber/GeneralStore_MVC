07366a04da432a3440bb98ab717715b9a5123dc3
Chris Kronberg <ckronber@gmail.com>
2021-06-22 09:59:34 -0400

    Add project files.

---
+++ GeneralStore MVC/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using GeneralStore_MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GeneralStore_MVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        //Add the application DB Context (link to the database)
+        private ApplicationDbContext _db = new ApplicationDbContext();
+
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View(_db.Products.ToList());
+        }
+
+        // GET: Product
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+
+    
+}
---
+++ GeneralStore MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GeneralStore_MVC.MvcApplication" Language="C#" %>
